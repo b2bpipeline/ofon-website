aa44815fd1994923259f7d2ada6c817bfe622d8d
Jossia Elang <81104672+nexonmegaxus@users.noreply.github.com>
2024-08-13 15:25:43 +0700

    fix: Error form

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxt/ui', '@vueuse/motion/nuxt', '@zadigetvoltaire/nuxt-gtm', '@nuxt/image', '@nuxtjs/seo'],
-    devtools: { enabled: true },
-    gtm: {
-        id: 'GTM-MJHXGQ5L',
-    },
-    app: {
-        pageTransition: { name: 'page', mode: 'out-in' },
-    },
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
-    css: ['~/assets/css/tailwind.css'],
-    colorMode: {
-        preference: 'light',
-    },
-    site: {
-        url: 'https://ofon-website.vercel.app/',
-        name: 'Ofon',
-        description: `Need to maximize productivity and minimize cost business telecommunications?`,
-        defaultLocale: 'en',
-    },
-    runtimeConfig: {
-        BACKEND_URL: process.env.NUXT_BACKEND_URL,
-    },
+  modules: ['@nuxt/ui', '@vueuse/motion/nuxt', '@zadigetvoltaire/nuxt-gtm', '@nuxt/image', '@nuxtjs/seo'],
+  devtools: { enabled: true },
+
+  gtm: {
+      id: 'GTM-MJHXGQ5L',
+  },
+
+  app: {
+      pageTransition: { name: 'page', mode: 'out-in' },
+  },
+
+  postcss: {
+      plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+      },
+  },
+
+  css: ['~/assets/css/tailwind.css'],
+
+  colorMode: {
+      preference: 'light',
+  },
+
+  site: {
+      url: 'https://ofon-website.vercel.app/',
+      name: 'Ofon',
+      description: `Need to maximize productivity and minimize cost business telecommunications?`,
+      defaultLocale: 'en',
+  },
+
+  runtimeConfig: {
+      BACKEND_URL: process.env.NUXT_BACKEND_URL,
+  },
+
+  compatibilityDate: '2024-08-13',
 });
